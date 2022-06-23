e1da7f3b20a3d8df5b60f3b1bd3bc7791627c89b
SamuelMorais030202 <107885526+SamuelMorais030202@users.noreply.github.com>
2022-06-23 14:18:35 -0300

    Update variaveisCompostas.js
    adicionei conteúdo na aula 15

--- curso2javaScript/aula15/variaveisCompostas.js
+++ curso2javaScript/aula15/variaveisCompostas.js
@@ -8,3 +8,12 @@ console.log(arr.length)
 for(let indice in arr){
     console.log(arr[indice])
 }
+
+arr.pop() // remove o último elemento
+console.log(arr)
+
+arr.push(0) // adiciona um elemento na última posição
+console.log(arr)
+
+arr.sort()
+console.log(arr) // coloca os elementos do array em ordem crescente
